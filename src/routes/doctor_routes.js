68b47d6beb587fa3a70a42f1f69725abe13730be
afodmat <afodisiacmwa@gmail.com>
2026-09-19 00:29:50 +0300

    update

--- src/routes/doctor_routes.js
+++ src/routes/doctor_routes.js
@@ -14,9 +14,9 @@ import { authenticate, authorize , isOwnerOrAdmin } from '../middleware/auth_mid
 const router = express.Router();
 
 // Public routes (require authentication)
-router.get( '/:id', authenticate, isOwnerOrAdmin('id', 'doctor'), getDoctorByIdController );
 router.get( '/available', authenticate, getDoctorsForAppointmentController );
 router.get( '/me', authenticate, getMyDoctorController );
+router.get( '/:id', authenticate, isOwnerOrAdmin('id', 'doctor'), getDoctorByIdController );
 // Admin only routes
 router.get('/', authenticate,authorize(['SUPER_ADMIN', 'ADMIN']), getAllDoctorsController);
 router.post( '/', authenticate, authorize(['ADMIN', 'SUPER_ADMIN']), createDoctorController );
